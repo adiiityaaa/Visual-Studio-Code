e3d4c2fff25985f856f2ce3b737ec0da9eec27da
adiiityaaa <adityak3653@gmail.com>
2023-01-12 17:58:28 +0530

    Advancements to Permutation Combination.

--- C++/PermutationCombination.cpp
+++ C++/PermutationCombination.cpp
@@ -3,17 +3,40 @@
 #include <windows.h>
 
 using namespace std;
-int main()
-{
-system("CLS");
-system("Color 0A");  
- int a;
- long factorial = 1.0;
- cout<<"Enter the number: ";
- cin>>a;
- for(int n=1; n <= a; ++n) {
+
+int fact(int x) {
+ int factorial = 1;
+ for(int n=1; n <= x; ++n) {
    factorial *= n; 
  }
-cout<<"Factorial is: " << factorial; 
-getch();
+ return factorial;
+}
+
+int main()
+{
+ int a, n, ans, r;
+ system("CLS");
+ system("Color 0A");
+ cout<<"Please select your operation:\n1. Permutation\n2. Combination\n3. Factorial of a Number\n";
+ cin>>a;  
+ switch(a) {
+  case 1: cout<<"Please enter N and R:\n";
+          cin>>n>>r;
+          if(n > 12 || r > 12) { cout<<"Values cannot be greater than 12 due to C++ restrictions."; exit(0); }
+          ans = fact(n)/(fact(n-r));
+          cout<<"Permutation where n = "<<n<<" and r = "<<r<<" is "<<ans<<"."; 
+  break;
+  case 2: cout<<"Please enter N and R:\n";
+          cin>>n>>r;
+          if(n > 12 || r > 12) { cout<<"Values cannot be greater than 12 due to C++ restrictions."; exit(0); }
+          ans = fact(n)/(fact(n-r)*fact(r));
+          cout<<"Combination where n = "<<n<<" and r = "<<r<<" is "<<ans<<"."; 
+  break;
+  case 3: cout<<"Please enter the number:\n";
+          cin>>n;
+          if(n > 12) { cout<<"Cannot calculate factorial of number greater than 12 due to C++ restrictions."; exit(0); } else { ans = fact(n); cout<<"Factorial of "<<n<<" is "<<ans; }
+  break;
+  default: cout<<"Please enter a valid option."; exit(0);
+  break;        
+ }
 }
